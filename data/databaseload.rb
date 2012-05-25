9a7d212c26b8d8f6eb4116437226383e7d24ca4a
Jeremy Stuckey <klash64@gmail.com>
2012-05-25 15:58:18 -0400

    Changed to accept the scrape file name as an argument.

--- data/databaseload.rb
+++ data/databaseload.rb
@@ -3,49 +3,56 @@ require_relative "databaseinit.rb"
 # require File.join(File.expand_path(".."), "ntoidatamapper.rb")
 # require 'databaseinit.rb'
 
-# Check if the database has been initialized
-if Team.count <= 0
-  initDatabase
-end
+# Check for scrape file passed in
+if ARGV[0]
+  # Get the scrape file name
+  scrapeFileName = ARGV[0]
+  
+  # Check if calling this script from the data folder or the project root
+  if Dir.pwd.include? "/data"
+    # Open game scrape file from this directory
+    scrapeFile = File.open(scrapeFileName)
+  else
+    # Open game scrape file from data directory
+    scrapeFile = File.open(File.expand_path("data/#{scrapeFileName}", Dir.pwd)) 
+  end
+  
+  # Check if the database has been initialized
+  if Team.count <= 0
+    initDatabase
+  end
+  
+  # Loop through each line of the scrape file
+  scrapeFile.each do |line|
+    # Split data by the delimiter  
+    gameData = line.split('***')
+    season = gameData[0]
+    gameType = gameData[1]
+    awayTeamAbbreviation = gameData[2]
+    homeTeamAbbreviation = gameData[3]
+    date = gameData[4]
+    gameNumber = gameData[5].gsub(/\n/, "")
+    
+    # Add game
+    newGame = Game.create(:gameID => gameNumber, :date => date, :gameType => gameType)
+    
+    # Add game to season
+    currentSeason = Season.first(:seasonID => season)
+    currentSeason.games << newGame
+    currentSeason.save
+    
+    # Get participating teams
+    awayTeam = Team.first(:teamID => awayTeamAbbreviation)
+    homeTeam = Team.first(:teamID => homeTeamAbbreviation)
+    
+    # Set teams for added game
+    newGame.awayTeam = awayTeam
+    newGame.homeTeam = homeTeam
+    newGame.save
+    
+    puts "Added to database #{line}"
+  end
+  
+  scrapeFile.close
 
-# Check if calling this script from the data folder or the project root
-if Dir.pwd.include? "/data"
-  # Open game scrape file from this directory
-  scrapeFile = File.open("gamescrape.txt")
-else
-  # Open game scrape file from data directory
-  scrapeFile = File.open(File.expand_path("data/gamescrape.txt", Dir.pwd)) 
 end
-
-# Loop through each line of the scrape file
-scrapeFile.each do |line|
-  # Split data by the delimiter  
-  gameData = line.split('***')
-  season = gameData[0]
-  gameType = gameData[1]
-  awayTeamAbbreviation = gameData[2]
-  homeTeamAbbreviation = gameData[3]
-  date = gameData[4]
-  gameNumber = gameData[5].gsub(/\n/, "")
-  
-  # Add game
-  newGame = Game.create(:gameID => gameNumber, :date => date, :gameType => gameType)
-  
-  # Add game to season
-  currentSeason = Season.first(:seasonID => season)
-  currentSeason.games << newGame
-  currentSeason.save
-  
-  # Get participating teams
-  awayTeam = Team.first(:teamID => awayTeamAbbreviation)
-  homeTeam = Team.first(:teamID => homeTeamAbbreviation)
-  
-  # Set teams for added game
-  newGame.awayTeam = awayTeam
-  newGame.homeTeam = homeTeam
-  newGame.save
-  
-  puts "Added to database #{line}"
-end
-
-scrapeFile.close
