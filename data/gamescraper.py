fceeed5c9541a6a1ac6f2611e4b0d3b78a9b500c
Jeremy Stuckey <klash64@gmail.com>
2013-06-29 13:42:41 -0400

    Changed to use html5lib. Uncommented 2013 playoffs.

--- data/gamescraper.py
+++ data/gamescraper.py
@@ -5,23 +5,23 @@ from bs4 import BeautifulSoup
 from bs4 import Tag
 
 def scrapeForSeason(seasonID, gameType):
-        
+
     # Open the file to write to
     if rootDir:
         scrapeFile = open('data/gamescrape.txt', 'a')
     else:
         scrapeFile = open('gamescrape.txt', 'a')
-    
+
     # Regular expression for parsing game links to extract the game ID
     gameLinkPattern = re.compile('/ice/recap\.htm\?id=')
-    
+
     print 'Connecting to nhl.com for season ' + seasonID + (', Playoffs' if gameType == '3' else '') + '...'
-    
+
     # Make some soup out of the nhl.com schedule page
-    soup = BeautifulSoup(urllib2.urlopen('http://www.nhl.com/ice/schedulebyseason.htm?season=' + seasonID + '&gameType=' + gameType + '&team=&network=&venue=').read())
-    
+    soup = BeautifulSoup(urllib2.urlopen('http://www.nhl.com/ice/schedulebyseason.htm?season=' + seasonID + '&gameType=' + gameType + '&team=&network=&venue=').read(), 'html5lib')
+
     print 'Scraping nhl.com schedule for season' + seasonID + '...'
-    
+
     # Get table elements with class == schedTbl
     scheduleTables = soup('table', {'class' : 'schedTbl'})
     if len(scheduleTables) > 1:
@@ -30,7 +30,7 @@ def scrapeForSeason(seasonID, gameType):
     else:
         # Only one schedule table
         scheduleTable = scheduleTables[0]
-    
+
     # Loop through the tr tags in the schedule table
     for row in scheduleTable.tbody('tr'):
         # Variables to scrape
@@ -38,10 +38,10 @@ def scrapeForSeason(seasonID, gameType):
         homeTeam = None
         awayTeam = None
         nhlGameID = None
-        
+
         # Get the td tags from the current tr tag
         tds = row('td')
-        
+
         # Loop through td tags and look for the date, away team abbreviation, home team abbreviation, and nhl game ID
         for col in tds:
             # td classes are:
@@ -50,13 +50,13 @@ def scrapeForSeason(seasonID, gameType):
             # - time
             # - tvInfo
             # - skedLinks
-            
+
             # Try to get the div that contains the game date
             dateDivs = col('div', {'class' : 'skedStartDateSite'})
             if dateDivs and not gameDate:
                 # Found the game date div (More than one. Use the first div)
                 gameDate = unicode(dateDivs[0].string)
-                
+
             # Try to get the div that contains the team name (either away or home)
             teamNameDivs = col('div', {'class' : 'teamName'})
             if teamNameDivs:
@@ -65,7 +65,7 @@ def scrapeForSeason(seasonID, gameType):
                     # Get the anchor inside the div that holds the team abbreviation
                     # The anchor should be the first child element of the div
                     teamNameAnchor = div.contents[0]
-                    
+
                     # Check if we actually got the anchor and that it has a rel attribute
                     # The rel attribute holds the team abbreviation
                     # The All Star Game tds have a different structure, so this check is necessary. Silly All Star Game...
@@ -83,40 +83,40 @@ def scrapeForSeason(seasonID, gameType):
                         if not awayTeam:
                             awayTeam = 'ATL'
                         elif not homeTeam:
-                            homeTeam = 'ATL'                      
+                            homeTeam = 'ATL'
             # Try to get the anchor that contains the game recap link
             # The recap link contains the nhl game ID
             skedLinkAnchors = col('a', {'class', 'btn'})
             if skedLinkAnchors:
-                # Loop through the anchors and look for the recap link 
+                # Loop through the anchors and look for the recap link
                 for anchor in skedLinkAnchors:
                     # Use the reg ex compiled above to check if it is a recap link
                     regExMatch = gameLinkPattern.match(anchor['href'])
                     if regExMatch:
                         # Extract the nhl game ID from the end of the recap link
                         nhlGameID = anchor['href'][regExMatch.end():]
-                        
+
         # Check if we parsed out all the info from the current tr
-        if gameDate and awayTeam and homeTeam and nhlGameID:              
+        if gameDate and awayTeam and homeTeam and nhlGameID:
             scrapeFile.write(seasonID + '***' + ('Regular' if gameType == '2' else 'Playoffs' ) + '***' + awayTeam + '***' + homeTeam + '***' + gameDate + '***' + nhlGameID + '\n')
             print 'Wrote to scrape file: ' + awayTeam + ' at ' + homeTeam + ' on ' + gameDate + ' (' + nhlGameID + ')'
-              
+
     # Close the file connection
     scrapeFile.close()
-    
+
 if __name__ == "__main__":
-    
+
     # Check if calling this script from the project root or the data folder
     rootDir = True
     if os.getcwd().find('/data') >= 0:
         rootDir = False
-    
+
     # Remove old scrape file
     if rootDir:
         os.system('rm data/gamescrape.txt')
     else:
         os.system('rm gamescrape.txt')
-    
+
     # Scrape game data for the following seasons
     scrapeForSeason('20072008', '2')
     scrapeForSeason('20072008', '3')
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     scrapeForSeason('20112012', '2')
     scrapeForSeason('20112012', '3')
     scrapeForSeason('20122013', '2')
-#    scrapeForSeason('20122013', '3')
+    scrapeForSeason('20122013', '3')
 
     # Call ruby script to load scraped data into database using DataMapper
     if rootDir:
