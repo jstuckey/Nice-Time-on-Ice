05dd5860feede76ed5b3a931a65cadf32660b206
jstuckey <klash64@gmail.com>
2013-09-30 18:13:55 -0400

    Set default season as 2013-2014.

--- configure.rb
+++ configure.rb
@@ -3,28 +3,28 @@ configure :production do
 end
 
 configure do
-  
+
   # Constants
-  CURRENTSEASON = "20122013"
-  
+  CURRENTSEASON = "20132014"
+
   # HTML Classes
-  SEASONLINKCLASS = "season" 
+  SEASONLINKCLASS = "season"
   TEAMLINKCLASS = "team"
   GAMELINKCLASS = "game"
   SITELINKCLASS = "link"
-  
+
   class Link
     attr_accessor :site
     attr_accessor :description
     attr_accessor :link
-    
+
     def initialize(site, description, link)
       @site = site
       @description = description
       @link = link
     end
   end
-  
+
   class APIResource
     attr_accessor :path
     attr_accessor :queryString
@@ -32,7 +32,7 @@ configure do
     attr_accessor :inputs
     attr_accessor :inputDescriptions
     attr_accessor :queryStringStartIndex
-    
+
     def initialize(path, queryString, description, inputs, inputDescriptions)
       @path = path
       @queryString = queryString
@@ -42,5 +42,5 @@ configure do
       @queryStringStartIndex = path.count '{'
     end
   end
-  
+
 end
