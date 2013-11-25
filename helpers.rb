9c96f574c54a27fe8200567f1f97966df9299549
jstuckey <klash64@gmail.com>
2013-11-25 18:11:04 -0500

    Changed Extra Skater link format.

--- helpers.rb
+++ helpers.rb
@@ -355,36 +355,6 @@ helpers do
       year = season.seasonID
       gameNumber = game.gameID
 
-      # Format away team name for Extra Skater site
-      awayTeam = game.awayTeam.name
-      awayTeamParts = awayTeam.split
-      awayTeamAbbrev = awayTeamParts[-1].downcase
-
-      # Maple Leafs and Bluejackets are special cases for Extra Skater
-      if awayTeamAbbrev == "leafs"
-        awayTeamAbbrev = "maple-" + awayTeamAbbrev
-      end
-      if awayTeamAbbrev == "bluejackets"
-        awayTeamAbbrev = "blue-jackets"
-      end
-
-      # Format home team name for Extra Skater site
-      homeTeam = game.homeTeam.name
-      homeTeamParts = homeTeam.split
-      homeTeamAbbrev = homeTeamParts[-1].downcase
-
-      # Maple Leafs and Bluejackets are special cases for Extra Skater
-      if homeTeamAbbrev == "leafs"
-        homeTeamAbbrev = "maple-" + homeTeamAbbrev
-      end
-      if homeTeamAbbrev == "bluejackets"
-        homeTeamAbbrev = "blue-jackets"
-      end
-
-      # Format game date for Extra Skater site
-      gameDate = game.date
-      gameDateString = Date.parse(gameDate).to_s
-
       newLink = Link.new("NHL.com", "Boxscore", "http://www.nhl.com/ice/boxscore.htm?id=#{gameNumber}")
       links << newLink
       newLink = Link.new("NHL.com", "Recap", "http://www.nhl.com/ice/recap.htm?id=#{gameNumber}")
@@ -407,7 +377,7 @@ helpers do
       links << newLink
       newLink = Link.new("NHL.com", "Shot Report", "http://www.nhl.com/scores/htmlreports/#{year}/SS#{gameNumber[-6, 6]}.HTM")
       links << newLink
-      newLink = Link.new("Extra Skater", "Advanced Stats", "http://www.extraskater.com/game/#{gameDateString}-#{awayTeamAbbrev}-#{homeTeamAbbrev}")
+      newLink = Link.new("Extra Skater", "Advanced Stats", "http://www.extraskater.com/game/#{year[0, 4]}#{game.gameType == "Playoffs" ? "p" : ""}/#{gameNumber[-6, 6]}")
       links << newLink
       newLink = Link.new("Nullisecund", "Even Strength Shooting", "http://nullisecund.us/nhl/game.php?id=#{gameNumber[-5, 5]}")
       links << newLink
@@ -423,7 +393,6 @@ helpers do
       links << newLink
       newLink = Link.new("Behind the Net", "Shot Timeline", "http://behindthenet.ca/charts/Shots_#{game.gameType == "Playoffs" ? "Playoffs_" : ""}#{year[0,4]}_#{gameNumber[-5, 5]}.php")
       links << newLink
-
 
     end
 
