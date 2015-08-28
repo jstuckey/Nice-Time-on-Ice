5c691b375d51d24682a8a483d5814637eb5a316e
jstuckey <klash64@gmail.com>
2015-08-27 22:14:53 -0400

    Add generator for creating Link objects
    This custom rails generator creates a model and test for a Link object.
    It will make adding all these Link objects much less repetitive.

---
+++ lib/generators/link/link_generator.rb
@@ -0,0 +1,21 @@
+class LinkGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  def create_model
+    template "model.rb.erb", "app/models/links/#{file_name}.rb"
+  end
+
+  def create_test
+    template "test.rb.erb", "test/models/links/#{test_file_name}.rb"
+  end
+
+  private
+
+  def model_name
+    file_name.camelize
+  end
+
+  def test_file_name
+    "#{file_name}_test"
+  end
+end
