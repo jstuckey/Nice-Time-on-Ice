e8153ddb53285069cebe9b10a7663e521fbcac48
jstuckey <klash64@gmail.com>
2015-08-30 16:11:31 -0400

    Change link generator to reflect new structure
    Now grouping links under sub-directories of the links directory. Changed
    generator to accept a sub_directory argument in addition to the
    file_name argument.

--- lib/generators/link/link_generator.rb
+++ lib/generators/link/link_generator.rb
@@ -1,23 +1,49 @@
-class LinkGenerator < Rails::Generators::NamedBase
+class LinkGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  class_option :comments, type: :boolean, default: true
+  argument :sub_directory, type: :string
+  argument :file_name,     type: :string
+  class_option :comments,  type: :boolean, default: true
+
+  def check_model_directory
+    empty_directory(model_path) unless Dir.exists?(model_path)
+  end
 
   def create_model
-    puts "Comment option: #{options.comments?}"
-    template "model.rb.erb", "app/models/links/#{file_name}.rb"
+    template "model.rb.erb", File.join(model_path, model_file_name)
+  end
+
+  def check_test_directory
+    empty_directory(test_path) unless Dir.exists?(test_path)
   end
 
   def create_test
-    template "test.rb.erb", "test/models/links/#{test_file_name}.rb"
+    template "test.rb.erb", File.join(test_path, test_file_name)
   end
 
   private
 
-  def model_name
-    file_name.camelize
+  def model_path
+    "app/models/links/#{sub_directory.underscore}"
+  end
+
+  def model_file_name
+    "#{file_name.underscore}.rb"
+  end
+
+  def test_path
+    "test/models/links/#{sub_directory.underscore}"
   end
 
   def test_file_name
-    "#{file_name}_test"
+    "#{file_name.underscore}_test.rb"
   end
+
+  def submodule_name
+    sub_directory.camelize
+  end
+
+  def class_name
+    file_name.camelize
+  end
+
 end
