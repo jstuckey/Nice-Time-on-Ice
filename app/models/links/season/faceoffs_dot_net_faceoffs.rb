0e0c3de5038989c28c0cffd1ae702d5c0360030b
jstuckey <klash64@gmail.com>
2015-09-03 19:27:31 -0400

    Add remaining season links
    All links under the season section of nicetimeonice.com are now
    accounted for.

---
+++ app/models/links/season/faceoffs_dot_net_faceoffs.rb
@@ -0,0 +1,24 @@
+class Links::Season::FaceoffsDotNetFaceoffs < Links::Base
+
+  def site_name
+    "Faceoffs.net"
+  end
+
+  def description
+    "Faceoffs"
+  end
+
+  def url
+    "http://faceoffs.net/team/#{team.abbreviation}?year=" \
+    "#{season.year_start}-#{season.year_end.to_s[2,2]}"
+  end
+
+  def group
+    6
+  end
+
+  def position
+    0
+  end
+
+end
