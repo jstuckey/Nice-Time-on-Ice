5f31549821c386480ac5a1ba6d85cfb9236cfef2
jstuckey <klash64@gmail.com>
2015-09-06 17:44:25 -0400

    Use team fixtures for link helper tests
    Added all teams as fixture. Use these fixtures instead of creating a new
    team record in the test.

--- test/models/links/helpers_test.rb
+++ test/models/links/helpers_test.rb
@@ -7,92 +7,92 @@ class Links::HelpersTest < ActiveSupport::TestCase
   attr_accessor :team, :season, :game, :game_type
 
   test "#ha_team_number should return 1 for Anaheim" do
-    self.team = Team.create(abbreviation: "ANA")
+    self.team = teams(:ducks)
     assert_equal 1, ha_team_number
   end
 
   test "#ha_team_number should return 23 for Arizona" do
-    self.team = Team.create(abbreviation: "ARI")
+    self.team = teams(:yotes)
     assert_equal 23, ha_team_number
   end
 
   test "#ha_team_number should return 3 for Boston" do
-    self.team = Team.create(abbreviation: "BOS")
+    self.team = teams(:bruins)
     assert_equal 3, ha_team_number
   end
 
   test "#ha_team_number should return 4 for Buffalo" do
-    self.team = Team.create(abbreviation: "BUF")
+    self.team = teams(:sabres)
     assert_equal 4, ha_team_number
   end
 
   test "#ha_team_number should return 5 for Carolina" do
-    self.team = Team.create(abbreviation: "CAR")
+    self.team = teams(:canes)
     assert_equal 5, ha_team_number
   end
 
   test "#ha_team_number should return 6 for Calgary" do
-    self.team = Team.create(abbreviation: "CGY")
+    self.team = teams(:flames)
     assert_equal 6, ha_team_number
   end
 
   test "#ha_team_number should return 7 for Chicago" do
-    self.team = Team.create(abbreviation: "CHI")
+    self.team = teams(:blackhawks)
     assert_equal 7, ha_team_number
   end
 
   test "#ha_team_number should return 9 for Colorado" do
-    self.team = Team.create(abbreviation: "COL")
+    self.team = teams(:avalanche)
     assert_equal 9, ha_team_number
   end
 
   test "#ha_team_number should return 8 for Columbus" do
-    self.team = Team.create(abbreviation: "CBJ")
+    self.team = teams(:jackets)
     assert_equal 8, ha_team_number
   end
 
   test "#ha_team_number should return 10 for Dallas" do
-    self.team = Team.create(abbreviation: "DAL")
+    self.team = teams(:stars)
     assert_equal 10, ha_team_number
   end
 
   test "#ha_team_number should return 11 for Detroit" do
-    self.team = Team.create(abbreviation: "DET")
+    self.team = teams(:wings)
     assert_equal 11, ha_team_number
   end
 
   test "#ha_team_number should return 12 for Edmonton" do
-    self.team = Team.create(abbreviation: "EDM")
+    self.team = teams(:oilers)
     assert_equal 12, ha_team_number
   end
 
   test "#ha_team_number should return 13 for Florida" do
-    self.team = Team.create(abbreviation: "FLA")
+    self.team = teams(:cats)
     assert_equal 13, ha_team_number
   end
 
   test "#ha_team_number should return 14 for Los Angeles" do
-    self.team = Team.create(abbreviation: "LAK")
+    self.team = teams(:kings)
     assert_equal 14, ha_team_number
   end
 
   test "#ha_team_number should return 15 for Minnesota" do
-    self.team = Team.create(abbreviation: "MIN")
+    self.team = teams(:wild)
     assert_equal 15, ha_team_number
   end
 
   test "#ha_team_number should return 16 for Montreal" do
-    self.team = Team.create(abbreviation: "MTL")
+    self.team = teams(:habs)
     assert_equal 16, ha_team_number
   end
 
   test "#ha_team_number should return 17 for Nashville" do
-    self.team = Team.create(abbreviation: "NSH")
+    self.team = teams(:preds)
     assert_equal 17, ha_team_number
   end
 
   test "#ha_team_number should return 18 for New Jersey" do
-    self.team = Team.create(abbreviation: "NJD")
+    self.team = teams(:devils)
     assert_equal 18, ha_team_number
   end
 
@@ -102,17 +102,17 @@ class Links::HelpersTest < ActiveSupport::TestCase
   end
 
   test "#ha_team_number should return 20 for NY Rangers" do
-    self.team = Team.create(abbreviation: "NYR")
+    self.team = teams(:rags)
     assert_equal 20, ha_team_number
   end
 
   test "#ha_team_number should return 21 for Ottawa" do
-    self.team = Team.create(abbreviation: "OTT")
+    self.team = teams(:sens)
     assert_equal 21, ha_team_number
   end
 
   test "#ha_team_number should return 22 for Philadelphia" do
-    self.team = Team.create(abbreviation: "PHI")
+    self.team = teams(:flyers)
     assert_equal 22, ha_team_number
   end
 
@@ -122,27 +122,27 @@ class Links::HelpersTest < ActiveSupport::TestCase
   end
 
   test "#ha_team_number should return 25 for San Jose" do
-    self.team = Team.create(abbreviation: "SJS")
+    self.team = teams(:sharks)
     assert_equal 25, ha_team_number
   end
 
   test "#ha_team_number should return 26 for St. Louis" do
-    self.team = Team.create(abbreviation: "STL")
+    self.team = teams(:blues)
     assert_equal 26, ha_team_number
   end
 
   test "#ha_team_number should return 27 for Tampa Bay" do
-    self.team = Team.create(abbreviation: "TBL")
+    self.team = teams(:bolts)
     assert_equal 27, ha_team_number
   end
 
   test "#ha_team_number should return 28 for Toronto" do
-    self.team = Team.create(abbreviation: "TOR")
+    self.team = teams(:leafs)
     assert_equal 28, ha_team_number
   end
 
   test "#ha_team_number should return 29 for Vancouver" do
-    self.team = Team.create(abbreviation: "VAN")
+    self.team = teams(:canucks)
     assert_equal 29, ha_team_number
   end
 
@@ -152,32 +152,32 @@ class Links::HelpersTest < ActiveSupport::TestCase
   end
 
   test "#ha_team_number should return 2 for Winnipeg" do
-    self.team = Team.create(abbreviation: "WPG")
+    self.team = teams(:jets)
     assert_equal 2, ha_team_number
   end
 
   test "#alternate_team_abbreviation handles Kings" do
-    self.team = Team.create(abbreviation: "LAK")
+    self.team = teams(:kings)
     assert_equal "L.A", alternate_team_abbreviation
   end
 
   test "#alternate_team_abbreviation handles Devils" do
-    self.team = Team.create(abbreviation: "NJD")
+    self.team = teams(:devils)
     assert_equal "N.J", alternate_team_abbreviation
   end
 
   test "#alternate_team_abbreviation handles Sharks" do
-    self.team = Team.create(abbreviation: "SJS")
+    self.team = teams(:sharks)
     assert_equal "S.J", alternate_team_abbreviation
   end
 
   test "#alternate_team_abbreviation handles Lighting" do
-    self.team = Team.create(abbreviation: "TBL")
+    self.team = teams(:bolts)
     assert_equal "T.B", alternate_team_abbreviation
   end
 
   test "#alternate_team_abbreviation handles other team" do
-    self.team = Team.create(abbreviation: "TOR")
+    self.team = teams(:leafs)
     assert_equal "TOR", alternate_team_abbreviation
   end
 
@@ -192,7 +192,7 @@ class Links::HelpersTest < ActiveSupport::TestCase
   end
 
   test "#team_in_playoffs? should return true if team made playoffs for a season" do
-    self.team = Team.create(name: "Blackhawks")
+    self.team = teams(:caps)
     self.season = Season.create()
 
     game = Game.create(playoffs: true, home_team: team)
@@ -202,7 +202,7 @@ class Links::HelpersTest < ActiveSupport::TestCase
   end
 
   test "#team_in_playoffs? should return false if team missed playoffs for a season" do
-    self.team = Team.create(name: "Flyers")
+    self.team = teams(:flyers)
     self.season = Season.create()
 
     game = Game.create(playoffs: false, home_team: team)
