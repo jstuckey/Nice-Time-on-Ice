d5777fcbc5e32cf2882321384f5c8f83012d321d
jstuckey <klash64@gmail.com>
2015-09-07 13:08:55 -0400

    Fix failing team tests
    Adding a validation on Game#game_number caused these two tests to break
    since the records could no longer be saved.

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -10,16 +10,16 @@ class TeamTest < ActiveSupport::TestCase
 
   test "Team#games should return combination of home and away games" do
     team = teams(:caps)
-    game1 = Game.create(away_team: team, date: 2.days.ago)
-    game2 = Game.create(home_team: team, date: 1.day.ago)
+    game1 = Game.create(away_team: team, date: 2.days.ago, game_number: 123)
+    game2 = Game.create(home_team: team, date: 1.day.ago, game_number: 456)
 
     assert_equal [game1, game2], team.games
   end
 
   test "Team#games should order games by date" do
     team = teams(:caps)
-    game1 = Game.create(away_team: team, date: 2.days.ago)
-    game2 = Game.create(home_team: team, date: 3.day.ago)
+    game1 = Game.create(away_team: team, date: 2.days.ago, game_number: 123)
+    game2 = Game.create(home_team: team, date: 3.day.ago, game_number: 456)
 
     assert_equal [game2, game1], team.games
   end
