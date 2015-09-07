3cbb8a84b39aad8434e889842b7af1dac1ba3f6f
jstuckey <klash64@gmail.com>
2015-09-06 22:22:31 -0400

    Add uniqueness validation for game_number
    We don't want duplicate games in the case that games are imported
    multiple times.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :season
   belongs_to :away_team, class_name: "Team", foreign_key: "away_team_id"
   belongs_to :home_team, class_name: "Team", foreign_key: "home_team_id"
+
+  validates :game_number, uniqueness: true
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -6,4 +6,6 @@ class GameTest < ActiveSupport::TestCase
   should belong_to :away_team
   should belong_to :home_team
 
+  should validate_uniqueness_of :game_number
+
 end
