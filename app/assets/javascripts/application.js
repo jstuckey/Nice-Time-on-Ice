b924eb24d9021948128f04bb7f001b942a3fb006
jstuckey <klash64@gmail.com>
2015-09-08 21:48:57 -0400

    Add javascripts and stylesheets
    Move over scripts and css from NTOI v1. Also setup the asset pipeline
    manifests. Had to add another pair of manifests for scripts and css
    specific to Internet Explorer compatibility.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//
+//= stub ./ie
+//= stub ie.js
+//
 //= require_tree .
+//= require_tree ../../../vendor/assets/javascripts
---
+++ app/assets/javascripts/ie.js
@@ -0,0 +1,4 @@
+// This is a manifest file that'll be compiled into
+// application.js only if the browser is IE 8 or lower
+//
+//= require_directory ./ie
