f449cea580ca6de9e73731889c2f984e8d418204
jstuckey <klash64@gmail.com>
2015-09-16 21:18:27 -0400

    Add MainController and route
    This just contains an index action that shows the main page of NTOI. It
    creates an instance of RequestContext, which translates the request
    params into ActiveRecord objects. That context is then passed to various
    presenter objects, which do most of the work.

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class MainControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:season_presenter)
+    assert_not_nil assigns(:team_presenter)
+    assert_not_nil assigns(:game_presenter)
+    assert_not_nil assigns(:season_link_presenter)
+    assert_not_nil assigns(:game_link_presenter)
+  end
+
+end
