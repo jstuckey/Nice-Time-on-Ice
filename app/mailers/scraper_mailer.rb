b292995d7d43d89f3cc5fc4f67050addebccaad7
jstuckey <klash64@gmail.com>
2015-10-17 14:50:18 -0400

    Add mailer for reporting scraping results
    This email contains the results of scraping NHL.com and saving the
    generated games.

---
+++ app/mailers/scraper_mailer.rb
@@ -0,0 +1,10 @@
+class ScraperMailer < ApplicationMailer
+  default to: "laichitor.lumpit@gmail.com"
+  default from: "scraper@nicetimeonice.com"
+
+  def results_email(results)
+    @results = Array(results)
+    mail(subject: "Scraping Results")
+  end
+
+end
---
+++ test/mailers/scraper_mailer_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ScraperMailerTest < ActionMailer::TestCase
+  test "results email should be setup correctly" do
+    email = ScraperMailer.results_email([])
+    assert_equal ["laichitor.lumpit@gmail.com"], email.to
+    assert_equal ["scraper@nicetimeonice.com"], email.from
+    assert_equal "Scraping Results", email.subject
+  end
+
+  test "email body should include passed results" do
+    results = ["Saved game 123", "Error saving game 456"]
+    email = ScraperMailer.results_email(results)
+
+    body_enumerator(email).each do |body|
+      assert_match /Saved game 123/, body
+      assert_match /Error saving game 456/, body
+    end
+  end
+
+  private
+
+  def body_enumerator(email)
+    email.body.parts.map(&:body).map(&:to_s)
+  end
+end
