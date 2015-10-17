ce5d9f10c7ca7eeb70de169b545ac0aa4e3280d0
jstuckey <klash64@gmail.com>
2015-10-17 16:57:34 -0400

    Move non-model objects to new poros directory
    Previously stashed all plain old ruby objects in the `models` directory
    along with actual ActiveRecord objects. This creates a new `poros`
    directory to house such objects.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module NiceTimeOnIce
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths << File.join(config.root, "app", "poros")
     config.autoload_paths << File.join(config.root, "app", "presenters")
     config.autoload_paths << File.join(config.root, "app", "models", "links")
   end
