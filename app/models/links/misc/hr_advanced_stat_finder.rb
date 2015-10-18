ba1912caf4a7804f021ef50938e2129e599a052f
jstuckey <klash64@gmail.com>
2015-10-18 14:51:44 -0400

    Add Hockey-Reference miscellaneous link
    Also increase height of sections again.

---
+++ app/models/links/misc/hr_advanced_stat_finder.rb
@@ -0,0 +1,23 @@
+class Links::Misc::HrAdvancedStatFinder < Links::Base
+
+  def site_name
+    "Hockey-Reference"
+  end
+
+  def description
+    "Advanced Stats Finder"
+  end
+
+  def url
+    "http://www.hockey-reference.com/play-index/tpbp_finder.cgi"
+  end
+
+  def group
+    9
+  end
+
+  def position
+    0
+  end
+
+end
