480a70d0d2e01c3e34fb4d397c16560d8d359541
jstuckey <klash64@gmail.com>
2015-12-29 11:18:43 -0500

    Add presence validations to game model
    While making the GameSaver object, I needed to cause a validation
    failure. I assumed setting one of the game's fields to nil would do the
    trick, but I found that I never added presence validations.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,10 +3,13 @@ class Game < ActiveRecord::Base
   belongs_to :away_team, class_name: "Team", foreign_key: "away_team_id"
   belongs_to :home_team, class_name: "Team", foreign_key: "home_team_id"
 
-  validates :game_number, uniqueness: true
+  validates :game_number, uniqueness: true, presence: true
+  validates :date, presence: true
+  validates :season_id, presence: true
+  validates :away_team_id, presence: true
+  validates :home_team_id, presence: true
 
   def to_s
     "#<Game: #{game_number}>"
   end
-
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -8,6 +8,12 @@ class GameTest < ActiveSupport::TestCase
 
   should validate_uniqueness_of :game_number
 
+  should validate_presence_of :game_number
+  should validate_presence_of :date
+  should validate_presence_of :season_id
+  should validate_presence_of :away_team_id
+  should validate_presence_of :home_team_id
+
   test "should have custom #to_s method" do
     assert_equal "#<Game: 2014021201>", games(:game_one).to_s
   end
