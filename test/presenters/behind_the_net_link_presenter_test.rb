beb869fba8d2a07ca7fe4994a6da735ccc8ce93e
jstuckey <jeremystuckey@gmail.com>
2017-12-11 21:06:31 -0500

    Fix test description to include correct site name
    The dangers of copy/paste

--- test/presenters/behind_the_net_link_presenter_test.rb
+++ test/presenters/behind_the_net_link_presenter_test.rb
@@ -12,14 +12,14 @@ class BehindTheNetLinkPresenterTest < ActiveSupport::TestCase
     @subject = BehindTheNetLinkPresenter.new(context)
   end
 
-  test "#names returns a list of HockeyViz page names for the game/team/season" do
+  test "#names returns a list of BTN page names for the game/team/season" do
     actual_names = @subject.names
     expected_names.zip(actual_names) do |expected_name, actual_name|
       assert_equal expected_name, actual_name
     end
   end
 
-  test "#links returns a list of HockeyViz links for the game/team/season" do
+  test "#links returns a list of BTN links for the game/team/season" do
     actual_links = @subject.links
     expected_links.zip(actual_links) do |expected_link, actual_link|
       assert_equal expected_link, actual_link
