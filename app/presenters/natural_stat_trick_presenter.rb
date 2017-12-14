d61d27a2b4d534050751493fed33ef0cc03020b4
jstuckey <jeremystuckey@gmail.com>
2017-12-13 20:44:08 -0500

    Fixup array styles in presenter
    Using %W[] for consistency except when the array only has one element
    (that feel like overkill).

--- app/presenters/natural_stat_trick_presenter.rb
+++ app/presenters/natural_stat_trick_presenter.rb
@@ -6,9 +6,6 @@ class NaturalStatTrickPresenter < LinkPresenterBase
   end
 
   def links
-    [
-    "http://naturalstattrick.com/game.php?" \
-      "season=#{season}&game=#{game_number_without_year_and_zero}"
-    ]
+   ["http://naturalstattrick.com/game.php?season=#{season}&game=#{game_number_without_year_and_zero}"]
   end
 end
