62872152f74f7c0c8bcdac412cf077ca625804db
jstuckey <jeremystuckey@gmail.com>
2018-01-06 21:03:56 -0500

    Remove redundant uses of self

--- test/poros/alias_attribute_test.rb
+++ test/poros/alias_attribute_test.rb
@@ -4,35 +4,35 @@ class AliasAttributeTest < ActiveSupport::TestCase
   attr_accessor :foo
 
   test "should respond to new attribute" do
-    refute self.respond_to?(:bar)
-    refute self.respond_to?(:bar=)
+    refute respond_to?(:bar)
+    refute respond_to?(:bar=)
 
-    self.class_eval do
+    class_eval do
       include AliasAttribute
       alias_attribute :bar, :foo
     end
 
-    assert self.respond_to?(:bar)
-    assert self.respond_to?(:bar=)
+    assert respond_to?(:bar)
+    assert respond_to?(:bar=)
   end
 
   test "should alias :foo= attribute to :bar attribute" do
-    self.class_eval do
+    class_eval do
       include AliasAttribute
       alias_attribute :bar, :foo
     end
 
     self.foo = "return of foo"
-    assert_equal "return of foo", self.bar
+    assert_equal "return of foo", bar
   end
 
   test "should alias :bar= attribute to :foo attribute" do
-    self.class_eval do
+    class_eval do
       include AliasAttribute
       alias_attribute :bar, :foo
     end
 
     self.bar = "return of bar"
-    assert_equal "return of bar", self.foo
+    assert_equal "return of bar", foo
   end
 end
