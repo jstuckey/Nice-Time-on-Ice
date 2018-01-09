02747e8abcf2ccd2c35f707727dfc5d7e3d68506
jstuckey <jeremystuckey@gmail.com>
2018-01-08 20:55:27 -0500

    Use nested module syntax for RuboCop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,11 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/pride'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
