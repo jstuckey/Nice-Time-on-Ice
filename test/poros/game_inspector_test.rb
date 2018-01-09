67424674f54c202fd42d2346f30e15f9ef7ab6df
jstuckey <jeremystuckey@gmail.com>
2018-01-08 20:50:10 -0500

    Remove commented out code for RuboCop

--- test/poros/game_inspector_test.rb
+++ test/poros/game_inspector_test.rb
@@ -19,51 +19,10 @@ class GameInspectorTest < ActiveSupport::TestCase
     assert_equal expected, GameInspector.new(2014).missing_games
   end
 
-#  test "should find missing post season game" do
-
-#    create_game(2014030111)
-#    create_game(2014030113)
-#
-#    assert_equal [2014030112], GameInspector.new(2014).missing_games
-#  end
-#
-#  test "should find multiple missing post season games in a row" do
-#    Game.destroy_all
-#    create_game(2014030111)
-#    create_game(2014030112)
-#    create_game(2014030116)
-#
-#    expected = [2014030113, 2014030114, 2014030115]
-#    assert_equal expected, GameInspector.new(2014).missing_games
-#  end
-#
-#  test "should find missing post season games across multiple rounds" do
-#    Game.destroy_all
-#    create_game(2014030111)
-#    create_game(2014030113)
-#    create_game(2014030211)
-#    create_game(2014030213)
-#    create_game(2014030311)
-#    create_game(2014030313)
-#    create_game(2014030411)
-#    create_game(2014030413)
-#
-#    expected = [2014030112, 2014030212, 2014030312, 2014030412]
-#    assert_equal expected, GameInspector.new(2014).missing_games
-#  end
-#
   test "should return empty list if no missing games" do
     Game.destroy_all
     create_game(2014020001)
     create_game(2014020002)
-#    create_game(2014030111)
-#    create_game(2014030112)
-#    create_game(2014030211)
-#    create_game(2014030212)
-#    create_game(2014030311)
-#    create_game(2014030312)
-#    create_game(2014030411)
-#    create_game(2014030412)
 
     assert_equal [], GameInspector.new(2014).missing_games
   end
