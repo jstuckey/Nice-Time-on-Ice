00eaeb1dd5b66519cb8e1695df04e07d15f56cff
jstuckey <jeremystuckey@gmail.com>
2018-01-25 21:17:05 -0500

    Add script to set city, name, and full_name
    This is a one-shot script to update fields for all records in the teams
    table. We will run this in production after deploy to migrate the data.

---
+++ lib/tasks/add_city_full_name_to_teams.rb
@@ -0,0 +1,185 @@
+Team.find_by_abbreviation("ANA").update_attributes(
+  city: "Anaheim",
+  name: "Ducks",
+  full_name: "Anaheim Ducks"
+)
+
+Team.find_by_abbreviation("ARI").update_attributes(
+  city: "Arizona",
+  name: "Coyotes",
+  full_name: "Arizona Coyotes"
+)
+
+Team.find_by_abbreviation("BOS").update_attributes(
+  city: "Boston",
+  name: "Bruins",
+  full_name: "Boston Bruins"
+)
+
+Team.find_by_abbreviation("BUF").update_attributes(
+  city: "Buffalo",
+  name: "Sabres",
+  full_name: "Buffalo Sabres"
+)
+
+Team.find_by_abbreviation("CGY").update_attributes(
+  city: "Calgary",
+  name: "Flames",
+  full_name: "Calgary Flames"
+)
+
+Team.find_by_abbreviation("CAR").update_attributes(
+  city: "Carolina",
+  name: "Hurricanes",
+  full_name: "Carolina Hurricanes"
+)
+
+Team.find_by_abbreviation("CHI").update_attributes(
+  city: "Chicago",
+  name: "Blackhawks",
+  full_name: "Chicago Blackhawks"
+)
+
+Team.find_by_abbreviation("COL").update_attributes(
+  city: "Colorado",
+  name: "Avalanche",
+  full_name: "Colorado Avalanche"
+)
+
+Team.find_by_abbreviation("CBJ").update_attributes(
+  city: "Columbus",
+  name: "Blue Jackets",
+  full_name: "Columbus Blue Jackets"
+)
+
+Team.find_by_abbreviation("DAL").update_attributes(
+  city: "Dallas",
+  name: "Stars",
+  full_name: "Dallas Stars"
+)
+
+Team.find_by_abbreviation("DET").update_attributes(
+  city: "Detroit",
+  name: "Red Wings",
+  full_name: "Detroit Red Wings"
+)
+
+Team.find_by_abbreviation("EDM").update_attributes(
+  city: "Edmonton",
+  name: "Oilers",
+  full_name: "Edmonton Oilers"
+)
+
+Team.find_by_abbreviation("FLA").update_attributes(
+  city: "Florida",
+  name: "Panthers",
+  full_name: "Florida Panthers"
+)
+
+Team.find_by_abbreviation("LAK").update_attributes(
+  city: "Los Angeles",
+  name: "Kings",
+  full_name: "Los Angeles Kings"
+)
+
+Team.find_by_abbreviation("MIN").update_attributes(
+  city: "Minnesota",
+  name: "Wild",
+  full_name: "Minnesota Wild"
+)
+
+Team.find_by_abbreviation("MTL").update_attributes(
+  city: "Montreal",
+  name: "Canadiens",
+  full_name: "Montreal Canadiens"
+)
+
+Team.find_by_abbreviation("NSH").update_attributes(
+  city: "Nashville",
+  name: "Predators",
+  full_name: "Nashville Predators"
+)
+
+Team.find_by_abbreviation("NJD").update_attributes(
+  city: "New Jersey",
+  name: "Devils",
+  full_name: "New Jersey Devils"
+)
+
+Team.find_by_abbreviation("NYI").update_attributes(
+  city: "New York",
+  name: "Islanders",
+  full_name: "New York Islanders"
+)
+
+Team.find_by_abbreviation("NYR").update_attributes(
+  city: "New York",
+  name: "Rangers",
+  full_name: "New York Rangers"
+)
+
+Team.find_by_abbreviation("OTT").update_attributes(
+  city: "Ottawa",
+  name: "Senators",
+  full_name: "Ottawa Senators"
+)
+
+Team.find_by_abbreviation("PHI").update_attributes(
+  city: "Philadelphia",
+  name: "Flyers",
+  full_name: "Philadelphia Flyers"
+)
+
+Team.find_by_abbreviation("PIT").update_attributes(
+  city: "Pittsburgh",
+  name: "Penguins",
+  full_name: "Pittsburgh Penguins"
+)
+
+Team.find_by_abbreviation("SJS").update_attributes(
+  city: "San Jose",
+  name: "Sharks",
+  full_name: "San Jose Sharks"
+)
+
+Team.find_by_abbreviation("STL").update_attributes(
+  city: "St. Louis",
+  name: "Blues",
+  full_name: "St. Louis Blues"
+)
+
+Team.find_by_abbreviation("TBL").update_attributes(
+  city: "Tampa Bay",
+  name: "Lightning",
+  full_name: "Tampa Bay Lightning"
+)
+
+Team.find_by_abbreviation("TOR").update_attributes(
+  city: "Toronto",
+  name: "Maple Leafs",
+  full_name: "Toronto Maple Leafs"
+)
+
+Team.find_by_abbreviation("VAN").update_attributes(
+  city: "Vancouver",
+  name: "Canucks",
+  full_name: "Vancouver Canucks"
+)
+
+Team.find_by_abbreviation("VGK").update_attributes(
+  city: "Vegas",
+  name: "Golden Knights",
+  full_name: "Vegas Golden Knights"
+)
+
+Team.find_by_abbreviation("WSH").update_attributes(
+  city: "Washington",
+  name: "Capitals",
+  full_name: "Washington Capitals"
+)
+
+Team.find_by_abbreviation("WPG").update_attributes(
+  city: "Winnipeg",
+  name: "Jets",
+  full_name: "Winnipeg Jets"
+)
