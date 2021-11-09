d808ff13911af5b8aa1f6a3c54aae7cbcaa84bff
jstuckey <jeremystuckey@gmail.com>
2021-11-09 11:43:39 -0500

    Use rails 5.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,6 @@ Bundler.require(*Rails.groups)
 module NiceTimeOnIce
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
   end
 end
