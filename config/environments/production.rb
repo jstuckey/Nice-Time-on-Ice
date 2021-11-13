c92a27485edfbc729d02ea7ea012432e7c829403
jstuckey <jeremystuckey@gmail.com>
2021-11-13 15:39:55 -0500

    Specify mem_cache_store instead of dalli_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :dalli_store,
+    config.cache_store = :mem_cache_store,
       ENV["MEMCACHEDCLOUD_SERVERS"].split(','),
       { username: ENV["MEMCACHEDCLOUD_USERNAME"],
         password: ENV["MEMCACHEDCLOUD_PASSWORD"] }
